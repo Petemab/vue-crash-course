d72a45bd2a86f24f180823f83737ac2e2a7bae40
Petemab <petemabennett@gmail.com>
2020-01-11 16:01:05 +0000

    Main functionality and components all there

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
     <div v-for="todo in todos" v-bind:key="todo.id">
-      <h3>{{ todo.title }}</h3>
+      <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)" />
     </div>
   </div>
 </template>
 
 <script>
+import TodoItem from "./TodoItem";
 export default {
   name: "Todos",
+  components: {
+    TodoItem
+  },
   props: ["todos"]
 };
 </script>
