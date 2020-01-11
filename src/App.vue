9de1fd21ef7a74f7d99ad96b444db691ee30d907
Petemab <petemabennett@gmail.com>
2020-01-11 17:10:20 +0000

    more or less working basic todo list

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@
 import Header from "./components/layout/Header";
 import Todos from "./components/Todos";
 import AddTodo from "./components/AddTodo";
+import axios from "axios";
 export default {
   name: "app",
   components: {
@@ -19,33 +20,33 @@ export default {
   },
   data() {
     return {
-      todos: [
-        {
-          id: 1,
-          title: "Todo One",
-          completed: false
-        },
-        {
-          id: 2,
-          title: "Todo Two",
-          completed: true
-        },
-        {
-          id: 3,
-          title: "Todo Three",
-          completed: false
-        }
-      ]
+      todos: []
     };
   },
   methods: {
     deleteTodo(id) {
-      this.todos = this.todos.filter(todo => todo.id !== id);
+      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`) 
+      .then(res => this.todos = res.data.filter(todo => todo.id !== id))
+      .catch(err => console.log(err)); // eslint-disable-line
     },
     addTodo(newTodo) {
-      this.todos = [...this.todos, newTodo];
+      const { title, completed } = newTodo;
+
+      axios.post('https://jsonplaceholder.typicode.com/todos', {
+        title,
+        completed
+      })
+      .then(res => this.todos =  [...this.todos, res.data])
+      .catch(err => console.log(err)); // eslint-disable-line
     }
-  }
+  },
+  created() {
+      axios
+        .get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+        // .then(res => console.log(res.data)) // eslint-disable-line
+        .then(res => this.todos = res.data)
+        .catch(err => console.log(err)); // eslint-disable-line
+    }
 };
 </script>
 
